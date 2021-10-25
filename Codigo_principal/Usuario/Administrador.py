882bcdfa0cc7ea436ca1cffbfd72561dcf43081e
Facundoks <92186156+Facundoks@users.noreply.github.com>
2021-10-25 20:14:23 -0300

    Update Administrador.py

--- Codigo_principal/Usuario/Administrador.py
+++ Codigo_principal/Usuario/Administrador.py
@@ -1,2 +1,9 @@
-class Anses():
-    pass
+class Administrador():
+    def bloquear(ciudanano_1):
+        pass
+    
+    def desbloquear(ciudadano_1):
+        pass
+    
+    def dar_alta(evento_1):
+        pass
