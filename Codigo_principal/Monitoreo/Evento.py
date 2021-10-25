ec3cd7316e860f20111a75646f81e375b1a54be7
Facundoks <92186156+Facundoks@users.noreply.github.com>
2021-10-25 20:26:09 -0300

    Update Evento.py

--- Codigo_principal/Monitoreo/Evento.py
+++ Codigo_principal/Monitoreo/Evento.py
@@ -1,2 +1,9 @@
 class Evento():
-    pass
+    def __innit__(self, tipo, coordenada, personas_e):
+        self.tipo = tipo
+        self.coordenada = coordenada
+        self.personas_e = personas_e
+        
+    def indicador_calor():
+        return self.personas_e
+                       
