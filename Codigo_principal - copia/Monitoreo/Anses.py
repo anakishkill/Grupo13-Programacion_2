fd62cf799e66a9dd32fe3ece453b5e7f6aee9654
Facundoks <92186156+Facundoks@users.noreply.github.com>
2021-11-07 02:13:33 -0300

    Update Anses.py

--- Codigo_principal - copia/Monitoreo/Anses.py
+++ Codigo_principal - copia/Monitoreo/Anses.py
@@ -1,5 +1,5 @@
-from io import open
 import csv
+
 
 class Manager:
     def validarCiudadano(self):
@@ -7,32 +7,39 @@ class Manager:
 
 
 class Anses:
+    #Diferenciamos si es ciudadano o admin con la variable tipo
+    def __init__(self, tipo):
+        self.tipo = tipo
+
     def confirmacion(self, dato, num):
-        with open("DB_usuario.csv", "r") as file:
+        with open(f"../Database/DB_{self.tipo}.csv", "r") as file:
             reader = csv.reader(file, delimiter=",")
             for linea in reader:
                 if dato == linea[num]:
                     return True
             return False
 
+    def confirmarNombre(self, nombre, tipo):
+        return Anses(tipo).confirmacion(nombre, 0)
 
-    def confirmarCuil(self, cuil):
-        return Anses().confirmacion(cuil, 0)
+    def confirmarCuil(self, cuil, tipo):
+        return Anses(tipo).confirmacion(cuil, 2)
 
-    def confirmarTelef(self, telef):
-        return Anses().confirmacion(telef, 1)
+    def confirmarTelef(self, telef, tipo):
+        return Anses(tipo).confirmacion(telef, 3)
 
-    def confirmarZona(self, zona):
-        return Anses().confirmacion(zona, 3)
+    def confirmarZona(self, zona, tipo):
+        return Anses(tipo).confirmacion(zona, 4)
 
-    def agregarDato(self, cuil, telef, zona):
-        a = Anses().confirmarCuil(cuil)
-        b = Anses().confirmarTelef(telef)
-        if a or b:
+    def agregarUsuario(self, nombre, contrasena, cuil, telef, zona, tipo):
+        a = Anses(tipo).confirmarCuil(cuil, tipo)
+        b = Anses(tipo).confirmarTelef(telef, tipo)
+        c = Anses(tipo).confirmarNombre(nombre, tipo)
+        if a or b or c:
             return print("Datos ya usados, intentelo de nuevo")
         else:
-            f = open("DB_usuario.csv", "a", newline="")
+            f = open("../Database/DB_ciudadano.csv", "a", newline="")
             writer = csv.writer(f)
-            tupla_nueva = (cuil, telef, zona)
+            tupla_nueva = (nombre, contrasena, cuil, telef, zona)
             writer.writerow(tupla_nueva)
             f.close()
