b882e14b17134bebca939ff347a481384222a14b
Facundoks <92186156+Facundoks@users.noreply.github.com>
2021-11-06 23:50:07 -0300

    Add files via upload

---
+++ Codigo_principal - copia/Monitoreo/App.py
@@ -0,0 +1,23 @@
+class App:
+    pass
+
+class Contactointeres:
+    pass
+
+
+class AministradorSolicitudes:
+    listSolicitudes = []
+    listSolicitudesAcep = []
+
+    def aceptar(self):
+        pass
+
+    def rechazar(self):
+        pass
+
+    def solicitudesRechazadas(self):
+        pass
+
+
+class Solicitud:
+    pass
